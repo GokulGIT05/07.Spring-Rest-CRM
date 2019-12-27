f5576f8e7c47470f84e18544c60ab8f9df8cc9e8
Gokul - Tortoise <gkgopig02@gmail.com>
2019-12-27 11:31:29 +0530

    Small Changes

--- src/main/java/com/luv2code/springdemo/entity/Customer.java
+++ src/main/java/com/luv2code/springdemo/entity/Customer.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+
 
 @Entity
 @Table(name="customer")
@@ -17,6 +20,7 @@ public class Customer {
 	private int id;
 	
 	@Column(name="first_name")
+	@NotEmpty(message = "Please provide a name")
 	private String firstName;
 	
 	@Column(name="last_name")
